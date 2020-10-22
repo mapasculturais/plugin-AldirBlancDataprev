bf925a91d5ca36d2e7e2fef7800d0ca1ca6da594
ErleBiazzio <erleibiazzio@gmail.com>
2020-10-22 11:01:41 -0300

    Commit inicial exportador Dataprev inciso 3

--- Plugin.php
+++ Plugin.php
@@ -15,6 +15,7 @@ class Plugin extends \AldirBlanc\PluginValidador
             
             'csv_inciso1' => require_once env('AB_CSV_INCISO1', __DIR__ . '/config-csv-inciso1.php'),
             'csv_inciso2' => require_once env('AB_CSV_INCISO2', __DIR__ . '/config-csv-inciso2.php'),
+            'csv_inciso3' => require_once env('AB_CSV_INCISO3', __DIR__ . '/config-csv-inciso3.php'),
         ];
 
         parent::__construct($config);
@@ -30,16 +31,23 @@ class Plugin extends \AldirBlanc\PluginValidador
         $app->hook('template(opportunity.single.header-inscritos):end', function () use($plugin, $app){
             $inciso1Ids = [$plugin->config['inciso1_opportunity_id']];
             $inciso2Ids = array_values($plugin->config['inciso2_opportunity_ids']);
-            $opportunities_ids = array_merge($inciso1Ids, $inciso2Ids);
+            $inciso3Ids = $plugin->config['inciso3_opportunity_ids'];
+            $opportunities_ids = array_merge($inciso1Ids, $inciso2Ids, $inciso3Ids);
             $requestedOpportunity = $this->controller->requestedEntity; //Tive que chamar o controller para poder requisitar a entity
             $opportunity = $requestedOpportunity->id;
+            
             if(($requestedOpportunity->canUser('@control')) && in_array($requestedOpportunity->id,$opportunities_ids) ) {
                 $app->view->enqueueScript('app', 'aldirblanc', 'aldirblanc/app.js');
                 if (in_array($requestedOpportunity->id, $inciso1Ids)){
                     $inciso = 1;
+
                 }
                 else if (in_array($requestedOpportunity->id, $inciso2Ids)){
                     $inciso = 2;
+
+                }else if(in_array($requestedOpportunity->id, $inciso3Ids)){
+                    $inciso = 3;
+
                 }
                 $this->part('aldirblanc/csv-button', ['inciso' => $inciso, 'opportunity' => $opportunity]);
             }
--- layouts/parts/aldirblanc/csv-button.php
+++ layouts/parts/aldirblanc/csv-button.php
@@ -53,7 +53,26 @@ else if ($inciso ==2){
     </edit-box>
 
     
-    <?php
-}
+    <?php }else if ($inciso == 3){ ?>
+    <?php 
+    $route = MapasCulturais\App::i()->createUrl('dataprev', 'export_inciso3');
+    ?>
+        <a class="btn btn-default download form-export-clear" ng-click="editbox.open('export-inciso3', $event)" rel="noopener noreferrer">CSV Dataprev</a> 
+        <!-- Formulario para cpf -->
+        <edit-box id="export-inciso3" position="top" title="<?php i::esc_attr_e('Exportar CSV Inciso III') ?>" cancel-label="Cancelar" close-on-cancel="true">
+            <form class="form-export-dataprev" action="<?=$route?>" method="POST">
+        
+                <label for="from">Data inícial</label>
+                <input type="date" name="from" id="from">
+                
+                <label for="from">Data final</label>  
+                <input type="date" name="to" id="to">
+                
+                <input type="hidden" name="opportunity" value="<?=$opportunity?>">
 
-?>
+                # Caso não queira filtrar entre datas, deixe os campos vazios.
+                <button class="btn btn-primary download" type="submit">Exportar</button>            
+            </form>
+        </edit-box>
+    <?php } ?>
+    
