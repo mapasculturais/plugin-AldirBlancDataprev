2d85039be970165b47d80938a82407891e6ca89f
Rafael Chaves Freitas <rafael.chaves.freitas@gmail.com>
2020-10-27 16:11:06 -0300

    adiciona o slug como prefixo do editBox para evitar conflitos

--- layouts/parts/aldirblanc/csv-button.php
+++ layouts/parts/aldirblanc/csv-button.php
@@ -5,10 +5,10 @@ if ($inciso == 1){
     $route = MapasCulturais\App::i()->createUrl('dataprev', 'export_inciso1');    
     ?>
     
-    <a class="btn btn-default download btn-export-cancel"  ng-click="editbox.open('export-inciso1', $event)" rel="noopener noreferrer">CSV Dataprev</a>
+    <a class="btn btn-default download btn-export-cancel"  ng-click="editbox.open('<?= $slug ?>-export-inciso1', $event)" rel="noopener noreferrer">CSV Dataprev</a>
 
     <!-- Formulário -->
-    <edit-box id="export-inciso1" position="top" title="<?php i::esc_attr_e('Exportar CSV Inciso I') ?>" cancel-label="Cancelar" close-on-cancel="true">
+    <edit-box id="<?= $slug ?>-export-inciso1" position="top" title="<?php i::esc_attr_e('Exportar CSV Inciso I') ?>" cancel-label="Cancelar" close-on-cancel="true">
         <form class="form-export-dataprev" action="<?=$route?>" method="POST">
       
             <label for="from">Data inícial</label>
@@ -27,16 +27,16 @@ if ($inciso == 1){
 else if ($inciso ==2){
     $route = MapasCulturais\App::i()->createUrl('dataprev', 'export_inciso2');   
     ?>
-    <a class="btn btn-default download form-export-clear" ng-click="editbox.open('export-inciso2', $event)" rel="noopener noreferrer">CSV Dataprev</a>
+    <a class="btn btn-default download form-export-clear" ng-click="editbox.open('<?= $slug ?>-export-inciso2', $event)" rel="noopener noreferrer">CSV Dataprev</a>
     
     <!-- Formulario para cpf -->
-    <edit-box id="export-inciso2" position="top" title="<?php i::esc_attr_e('Exportar CSV Inciso II') ?>" cancel-label="Cancelar" close-on-cancel="true">
+    <edit-box id="<?= $slug ?>-export-inciso2" position="top" title="<?php i::esc_attr_e('Exportar CSV Inciso II') ?>" cancel-label="Cancelar" close-on-cancel="true">
         <form class="form-export-dataprev" action="<?=$route?>" method="POST">
       
             <label for="from">Data inícial</label>
             <input type="date" name="from" id="from">
             
-            <label for="from">Data final</label>  
+            <label for="to">Data final</label>  
             <input type="date" name="to" id="to">            
 
             <label for="type">Tipo de exportação (CPF ou CNPJ)</label>
@@ -57,15 +57,15 @@ else if ($inciso ==2){
     <?php 
     $route = MapasCulturais\App::i()->createUrl('dataprev', 'export_inciso3');
     ?>
-        <a class="btn btn-default download form-export-clear" ng-click="editbox.open('export-inciso3', $event)" rel="noopener noreferrer">CSV Dataprev</a> 
+        <a class="btn btn-default download form-export-clear" ng-click="editbox.open('<?= $slug ?>-export-inciso3', $event)" rel="noopener noreferrer">CSV Dataprev</a> 
         <!-- Formulario para cpf -->
-        <edit-box id="export-inciso3" position="top" title="<?php i::esc_attr_e('Exportar CSV Inciso III') ?>" cancel-label="Cancelar" close-on-cancel="true">
+        <edit-box id="<?= $slug ?>-export-inciso3" position="top" title="<?php i::esc_attr_e('Exportar CSV Inciso III') ?>" cancel-label="Cancelar" close-on-cancel="true">
             <form class="form-export-dataprev" action="<?=$route?>" method="POST">
         
                 <label for="from">Data inícial</label>
                 <input type="date" name="from" id="from">
                 
-                <label for="from">Data final</label>  
+                <label for="to">Data final</label>  
                 <input type="date" name="to" id="to">
                 
                 <input type="hidden" name="opportunity" value="<?=$opportunity?>">
